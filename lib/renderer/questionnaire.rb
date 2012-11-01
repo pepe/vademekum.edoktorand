1d7e37fec42802acbe1c843f9bfd26409d8991d1
Josef Pospíšil <josef.pospisil@laststar.eu>
2012-11-01 21:35:23 +0100

    Restyled questionnaire

--- lib/renderer/questionnaire.rb
+++ lib/renderer/questionnaire.rb
@@ -13,11 +13,17 @@ module Renderer
                'radio'
              end
       name  = qa[0].parameterize
-      res = "<p>#{quest}</p>"
+      res = "<p class='question'>#{quest}</p>"
       Array(qa[2]).each do |value|
-        res.concat "<label>#{value}" unless type == "text"
-        res.concat "<input type='#{type}' name='#{name}' value='#{value}'/>"
-        res.concat "</label>" unless type == "text"
+        if type == "text"
+          res.concat "<textarea class='answer' placeholder='#{value}' name='#{name}'></textarea>"
+        else
+          res.concat "<div class='answer'>"
+          res.concat "<label>"
+          res.concat "<input type='#{type}' name='#{name}' value='#{value}'/>"
+          res.concat "&nbsp;#{value}</label>"
+          res.concat "</div>"
+        end
       end
 
       return res
--- spec/lib/questionnaire_renderer_spec.rb
+++ spec/lib/questionnaire_renderer_spec.rb
@@ -7,17 +7,17 @@ describe Renderer::Questionnaire do
 
   it "renders check box question" do
     question(['Are you sure?', 'check', ['yes']]).should ==
-"<p>Are you sure?</p><label>yes<input type='checkbox' name='are-you-sure' value='yes'/></label>"
+      "<p class='question'>Are you sure?</p><div class='answer'><label><input type='checkbox' name='are-you-sure' value='yes'/>&nbsp;yes</label></div>"
   end
 
   it "renders text field question" do
     question(['Are you sure?', 'write', 'yes I am']).should ==
-"<p>Are you sure?</p><input type='text' name='are-you-sure' value='yes I am'/>"
+      "<p class='question'>Are you sure?</p><textarea class='answer' placeholder='yes I am' name='are-you-sure'></textarea>"
   end
 
   it "renders radio box field question" do
     question(['Color?', 'choose', ['red', 'simply red']]).should ==
-"<p>Color?</p><label>red<input type='radio' name='color' value='red'/></label><label>simply red<input type='radio' name='color' value='simply red'/></label>"
+      "<p class='question'>Color?</p><div class='answer'><label><input type='radio' name='color' value='red'/>&nbsp;red</label></div><div class='answer'><label><input type='radio' name='color' value='simply red'/>&nbsp;simply red</label></div>"
   end
 end
 
