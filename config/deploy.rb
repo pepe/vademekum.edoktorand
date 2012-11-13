48b01e9ca18d9d09efe5db90fdcdf9d1031f16aa
Josef Pospíšil <josef.pospisil@laststar.eu>
2012-11-13 21:31:40 +0100

    Add mina deploy

---
+++ config/deploy.rb
@@ -0,0 +1,53 @@
+require 'mina/bundler'
+require 'mina/git'
+#require 'mina/rvm'
+
+set :domain, 'litery.laststar.eu'
+set :deploy_to, '/home/deploy/vademekum.laststar.eu'
+set :repository, 'git://github.com/pepe/vademekum.edoktorand'
+set :branch, 'master'
+
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, ['log']
+
+# Optional settings:
+set :user, 'deploy'    # Username in the server to SSH to.
+#   set :port, '30000'     # SSH port number.
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+ # invoke :'rvm:use[ruby-1.9.3-p327@vademekum]'
+end
+
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+    # Put things that will set up an empty directory into a fully set-up
+    # instance of your project.
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+
+    to :launch do
+      queue "touch #{deploy_to}/tmp/restart.txt"
+    end
+  end
+end
+
+# For help in making your deploy script, see the Mina documentation:
+#
+#  - http://nadarei.co/mina
+#  - http://nadarei.co/mina/tasks
+#  - http://nadarei.co/mina/settings
+#  - http://nadarei.co/mina/helpers
+
