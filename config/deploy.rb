bd49fec1a440b0f245afcc9d27578da09e9dc71e
Josef Pospíšil <josef.pospisil@laststar.eu>
2013-02-21 14:59:23 +0100

    Fix motto and deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,31 +1,19 @@
 require 'mina/bundler'
 require 'mina/git'
-#require 'mina/rvm'
+require 'mina/rvm'
 
-set :domain, 'litery.laststar.eu'
-set :deploy_to, '/home/deploy/vademekum.laststar.eu'
+set :domain, 'edoktorand.czu.cz'
+set :deploy_to, '/home/deploy/vademekum.edoktorand.czu.cz'
 set :repository, 'git://github.com/pepe/vademekum.edoktorand'
 set :branch, 'master'
 
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['log']
+set :user, 'deploy'
 
-# Optional settings:
-set :user, 'deploy'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
 task :environment do
- # invoke :'rvm:use[ruby-1.9.3-p327@vademekum]'
+  invoke :'rvm:use[ruby-1.9.3-p327@vademekum]'
 end
 
-
-# Put any custom mkdir's in here for when `mina setup` is ran.
-# For Rails apps, we'll make some of the shared paths that are shared between
-# all releases.
-set :shared_paths, ['public/sheets']
+set :shared_paths, ['log', 'public/sheets']
 
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
@@ -37,22 +25,13 @@ end
 desc "Deploys the current version to the server."
 task :deploy => :environment do
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
 
     to :launch do
-      queue "touch #{deploy_to}/tmp/restart.txt"
+      queue "thin -C #{deploy_to}/shared/thin.yml restart"
     end
   end
 end
-
-# For help in making your deploy script, see the Mina documentation:
-#
-#  - http://nadarei.co/mina
-#  - http://nadarei.co/mina/tasks
-#  - http://nadarei.co/mina/settings
-#  - http://nadarei.co/mina/helpers
 
